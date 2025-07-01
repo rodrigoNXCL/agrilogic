e4f23d16cea6d4beb6bef4eca50ad06b32de1dac
rodrigoNXCL <nxcl.servicios@gmail.com>
2025-07-01 16:43:26 -0400

    feat(ui): añadir Navbar con logo de M y M Asesorías

---
+++ mym-web/src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+// src/components/Navbar.jsx
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <nav className="py-4 px-6 bg-white flex items-center">
+      <img
+        src="/logobase_mym_sf.png"
+        alt="Logo M y M Asesorías"
+        className="h-12 w-auto"
+      />
+    </nav>
+  );
+}
--- mym-web/src/pages/App.jsx
+++ mym-web/src/pages/App.jsx
@@ -1,11 +1,14 @@
 import React from "react";
+import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
+import ContactForm from "../components/ContactForm";
 
 export default function App() {
   return (
     <>
+      <Navbar />
       <Hero />
-      {/* ...otros componentes */}
+      <ContactForm />
     </>
   );
 }
